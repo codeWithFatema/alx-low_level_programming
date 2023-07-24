480ab7b0aede597cfc3c994b8f08723292e02797
FatemaNunow <fatema14384@gmail.com>
2023-07-24 12:13:09 -0700

    pp

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include "main.h"
+
+void _puts(char *);
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
+	char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+	str = "Holberton!";
+	_puts(str);
+	return (0);
 }
