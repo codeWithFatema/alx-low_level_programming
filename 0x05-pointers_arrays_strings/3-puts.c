eaa3aff8043caf5bae7e11407b257e1050cd292d
FatemaNunow <fatema14384@gmail.com>
2023-07-24 11:44:14 -0700

    u

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include<sdtio.h>
 
-/** 
- * _puts - prints string
- * 
- * @str: input string 
- * Return: no return
- */
-void _puts(char *str) 
-{
-    while (*str != '\0')
-    {
-        _putchar(*str);
+  /** 
+   * _puts - prints string
+   * 
+   * @str: input string 
+   * Return: no return
+   */
+   void _puts(char *str) 
+  {
+     while (*str != '\0' ) 
+ {
+    _putchar(*str);
         str++;
-    } 
-    _putchar('\n'); 
-}
+} 
+     _putchar('\n'); 
+  }
