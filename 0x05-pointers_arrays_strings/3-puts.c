20d171468df297d2850653b777737cfab4b098ef
FatemaNunow <fatema14384@gmail.com>
2023-07-24 12:10:38 -0700

    f

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
-#include<sdtio.h>
+#include <stdio.h>
+#include "main.h"
 
-  /** 
-   * _puts - prints string
-   * 
-   * @str: input string 
-   * Return: no return
-   */
-   void _puts(char *str) 
-  {
-     while (*str != '\0' ) 
- {
-    _putchar(*str);
-        str++;
-} 
-     _putchar('\n'); 
-  }
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
