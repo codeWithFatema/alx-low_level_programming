f4414b6ae16c870b911a872adbe00d9f1bd09e1f
FatemaNunow <fatema14384@gmail.com>
2023-07-24 11:41:58 -0700

    update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include<stdio.h>
 
 /** 
  * _puts - prints string
@@ -11,7 +11,7 @@ void _puts(char *str)
     while (*str != '\0')
     {
         _putchar(*str);
-        str++
+        str++;
     } 
     _putchar('\n'); 
 }
