72fc34be0913ff02a2f4e85d1d0746c061394175
FatemaNunow <fatema14384@gmail.com>
2023-07-24 11:33:43 -0700

    fd

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-  /** 
-   * _strlen - find the length of a string 
-   * @s: pointer to the string to check 
-   * Return: void
-   */
-   int _strlen(char *s) 
-  {
-     int swap;
+/** 
+ * _strlen - find the length of a string 
+ * @s: pointer to the string to check 
+ * Return: length of the string
+ */
+int _strlen(char *s) 
+{
+    int i = 0;
+    while (s[i])
+        i++;
 
-     int i = 0;
-     while (s[i]) 
-            i++;
-
-     return (i);
-  } 
+    return i;
+}
