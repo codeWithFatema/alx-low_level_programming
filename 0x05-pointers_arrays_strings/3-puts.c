6ed067acc893b099355fc963882d4fe6c402e497
FatemaNunow <fatema14384@gmail.com>
2023-07-24 12:39:54 -0700

    fa

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
 #include "main.h"
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	int i;
 
-  /** 
-   * _puts - prints string
-   * 
-   * @str: input string 
-   * Return: no return
-   */
-   void _puts(char *str) 
-  {
-     while (*str != '\0' ) 
- {
-    _putchar(*str);
-        str++;
-} 
-     _putchar('\n'); 
-  }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
