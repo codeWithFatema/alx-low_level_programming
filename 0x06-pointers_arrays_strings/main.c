c693dfbf93c52156a6c163ee9098e3476a836276
FatemaNunow <fatema14384@gmail.com>
2023-07-27 10:44:15 -0700

    jds

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    char n1[] = "123";
-    char n2[] = "456";
-    char result[100];
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
-    char *res = infinite_add(n1, n2, result, 100);
-    printf("Result: %s\n", res);
-
-    return 0;
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
