d6767db9c05699ee739ea133e737d32207f4d8e1
FatemaNunow <fatema14384@gmail.com>
2023-07-27 10:58:29 -0700

    fsda

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,56 +5,43 @@
  * infinite_add - add 2 strings.
  * @n1: string1.
  * @n2: string2.
- * @r: buffer
- * @size_r: buffer size
- * Return: String with all letters in ROT13 base.
+ * @r: buffer.
+ * @size_r: buffer size.
+ * Return: String with the sum of n1 and n2.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
+    int a_len = 0, b_len = 0, carry = 0, a, b, sum, max_len;
 
     while (n1[a_len] != '\0')
         a_len++;
     while (n2[b_len] != '\0')
         b_len++;
-    if (a_len > b_len)
-        biggest = a_len;
-    else
-        biggest = b_len;
-    if ((biggest + 1) >= size_r)
+
+    max_len = (a_len > b_len) ? a_len : b_len;
+
+    if ((a_len + b_len + 1) >= size_r)
         return (0);
-    r[biggest + 1] = '\0';
 
-    while (biggest >= 0)
+    r[max_len + 1] = '\0';
+
+    while (max_len >= 0)
     {
-        a = (n1[a_len - 1] - '0');
-        b = (n2[b_len - 1] - '0');
+        a = (a_len > 0) ? (n1[a_len - 1] - '0') : 0;
+        b = (b_len > 0) ? (n2[b_len - 1] - '0') : 0;
 
-        if (a_len == 0 && b_len == 0)
-            sum = carry;
-        else if (a_len == 0)
-            sum = b + carry;
-        else if (b_len == 0)
-            sum = a + carry;
-        else
-            sum = a + b + carry;
-
+        sum = a + b + carry;
         carry = sum / 10;
         sum = sum % 10 + '0';
 
-        r[biggest] = sum;
+        r[max_len] = sum;
         a_len--;
         b_len--;
-        biggest--;
+        max_len--;
     }
 
-    // Check if there's enough space for the carry value
-    if (carry > 0 && (biggest + 1) < size_r)
-    {
-        r[biggest] = carry + '0';
-        return r;
-    }
-
-    // If carry value cannot be stored, return 0
-    return (0);
+    if (*r == '0')
+        return (r + 1);
+    else
+        return (r);
 }
