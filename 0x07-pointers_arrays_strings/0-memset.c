9b42e67e9818c19c8e4eb3a4c9bb4007fe3c17d0
FatemaNunow <fatema14384@gmail.com>
2023-07-31 02:07:43 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
-#include<string.h>
-char* _memset(char* s, char b, unsigned int n);
-int main(){
-char buffer[10]; //creates a buffer of size 10 bytes 
-    char* result;
-    // Fill the buffer with the constant byte 'A' (ASCII value 65)
-    result = _memset(buffer, 'A', 10);
-    // Print the buffer to check the result
-    for (int i = 0; i < 10; i++) {
-        printf("%c ", buffer[i]);
+#include <stddef.h>
+
+char* _memset(char* s, char b, unsigned int n) {
+    char* ptr = s; // Keep a reference to the start of the memory area
+
+    // Fill the first n bytes of the memory area with the constant byte b
+    for (unsigned int i = 0; i < n; i++) {
+        *s = b;
+        s++;
     }
-    return 0;
+
+    return ptr; // Return the pointer to the memory area s
 }
